2aa1d9ce65ca0315e7b7d23e7a2c307fe8294c49
Sujit R. Chhetri <sujitchhetri@gmail.com>
2018-11-02 11:59:07 -0700

    added models and changed parameters

--- example/example.py
+++ example/example.py
@@ -5,43 +5,155 @@ try: import cPickle as pickle
 except: import pickle
 import numpy as np
 
-from gem.utils      import graph_util, plot_util
-from gem.evaluation import visualize_embedding as viz
-from gem.evaluation import evaluate_graph_reconstruction as gr
+#import helper libraries
+from dynamicgem.utils      import graph_util, plot_util, dataprep_util
+from dynamicgem.evaluation import visualize_embedding as viz
+from dynamicgem.evaluation import evaluate_graph_reconstruction as gr
+from dynamicgem.graph_generation import dynamic_SBM_graph as sbm
 
-from gem.embedding.gf       import GraphFactorization
-from gem.embedding.hope     import HOPE
-from gem.embedding.lap      import LaplacianEigenmaps
-from gem.embedding.lle      import LocallyLinearEmbedding
-from gem.embedding.node2vec import node2vec
-from gem.embedding.sdne     import SDNE
+#import the methods
+from dynamicgem.embedding.ae_static    import AE
+from dynamicgem.embedding.dynamicTriad import dynamicTriad
+from dynamicgem.embedding.TIMERS       import TIMERS
+from dynamicgem.embedding.dynAE        import DynAE
+from dynamicgem.embedding.dynRNN       import DynRNN
+from dynamicgem.embedding.dynAERNN     import DynAERNN
 
 
-# File that contains the edges. Format: source target
-# Optionally, you can add weights as third column: source target weight
-file_prefix = 'sbm.gpickle'
-# Specify whether the edges are directed
-isDirected = True
+# Parameters for Stochastic block model graph
+# Todal of 1000 nodes
+node_num           = 1000
+# Test with two communities
+community_num      = 2
+# At each iteration migrate 10 nodes from one community to the another
+node_change_num    = 10
+# Length of total time steps the graph will dynamically change
+length             = 10
+# output directory for result
+outdir = './output'
+testDataType = 'sbm_cd'
+#Generate the dynamic graph
+dynamic_sbm_series = sbm.get_community_diminish_series_v2(node_num, 
+                                                          community_num, 
+                                                          length, 
+                                                          1, #comminity ID to perturb
+                                                          node_change_num)
+graphs     = [g[0] for g in dynamic_sbm_series]
 
-# Load graph
-G = nx.read_gpickle(file_prefix)
-node_colors = pickle.load(
-    open('sbm_node_labels.pickle', 'rb')
-)
 node_colors_arr = [None] * node_colors.shape[0]
 for idx in range(node_colors.shape[0]):
     node_colors_arr[idx] = np.where(node_colors[idx, :].toarray() == 1)[1][0]
 
-models = []
+models  = []
+# parameters for the dynamic embedding
+# dimension of the embedding
+dim_emb = 128
+
 # Load the models you want to run
-models.append(GraphFactorization(d=128, max_iter=1000, eta=1 * 10**-4, regu=1.0, data_set='sbm'))
-models.append(HOPE(d=256, beta=0.01))
-models.append(LaplacianEigenmaps(d=128))
-models.append(LocallyLinearEmbedding(d=128))
-models.append(node2vec(d=182, max_iter=1, walk_len=80, num_walks=10, con_size=10, ret_p=1, inout_p=1, data_set='sbm'))
-models.append(SDNE(d=128, beta=5, alpha=1e-5, nu1=1e-6, nu2=1e-6, K=3,n_units=[500, 300,], rho=0.3, n_iter=30, xeta=0.001,n_batch=500,
-                modelfile=['enc_model.json', 'dec_model.json'],
-                weightfile=['enc_weights.hdf5', 'dec_weights.hdf5']))
+models.append(AE(d          = dim_emb, 
+                 beta       = 5, 
+                 nu1        = 1e-6, 
+                 nu2        = 1e-6,
+                 K          = 3, 
+                 n_units    = [500, 300, ],
+                 n_iter     = epochs, 
+                 xeta       = 1e-4,
+                 n_batch    = 100,
+                 modelfile  = ['./intermediate/enc_modelsbm.json',
+                             './intermediate/dec_modelsbm.json'],
+                 weightfile = ['./intermediate/enc_weightssbm.hdf5',
+                             './intermediate/dec_weightssbm.hdf5']))
+
+datafile  = dataprep_util.prep_input_dynTriad(graphs, length, testDataType)
+models.append(dynamicTriad(niters     = 20,
+                 starttime  = 0,
+                 datafile   = datafile,
+                 batchsize  = 1000,
+                 nsteps     = length,
+                 embdim     = dim_emb,
+                 stepsize   = 1,
+                 stepstride = 1,
+                 outdir     = outdir,
+                 cachefn    = '/tmp/'+ testDataType,
+                 lr         = 0.1,
+                 beta       = [0.1,0.1],
+                 negdup     = 1,
+                 datasetmod = 'core.dataset.adjlist',
+                 trainmod   = 'dynamicgem.dynamictriad.core.algorithm.dynamic_triad',
+                 pretrain_size = length,
+                 sampling_args = {},
+                 validation = 'link_reconstruction',
+                 datatype   = testDataType,
+                 scale      = 1,
+                 classifier = 'lr',
+                 debug      = False,
+                 test       = 'link_predict',
+                 repeat     = 1,
+                 resultdir  = outdir,
+                 testDataType = testDataType,
+                 clname       = 'lr',
+                 node_num     = node_num ))
+
+datafile  = dataprep_util.prep_input_TIMERS(graphs, length, testDataType) 
+models.append(TIMERS(K        = dim_emb, 
+                 Theta         = 0.5, 
+                 datafile      = datafile,
+                 length        =  length,
+                 nodemigration = node_change_num,
+                 resultdir     = output,
+                 datatype      = testDataType))
+
+models.append(DynAE(d           = dim_emb,
+                 beta           = 5,
+                 n_prev_graphs  = 2,
+                 nu1            = 1e-6,
+                 nu2            = 1e-6,
+                 n_units        = [500, 300,],
+                 rho            = 0.3,
+                 n_iter         = 250,
+                 xeta           = 1e-4,
+                 n_batch        = 100,
+                 modelfile      = ['./intermediate/enc_model_dynAE.json', 
+                                   './intermediate/dec_model_dynAE.json'],
+                 weightfile     = ['./intermediate/enc_weights_dynAE.hdf5', 
+                                   './intermediate/dec_weights_dynAE.hdf5'],
+                 savefilesuffix = "testing" ))
+
+models.append(DynRNN(d        = dim_emb,
+                beta           = 5,
+                n_prev_graphs  = 2,
+                nu1            = 1e-6,
+                nu2            = 1e-6,
+                n_enc_units    = [500,300],
+                n_dec_units    = [500,300],
+                rho            = 0.3,
+                n_iter         = 250,
+                xeta           = 1e-3,
+                n_batch        = 100,
+                modelfile      = ['./intermediate/enc_model_dynRNN.json', 
+                                  './intermediate/dec_model_dynRNN.json'],
+                weightfile     = ['./intermediate/enc_weights_dynRNN.hdf5', 
+                                  './intermediate/dec_weights_dynRNN.hdf5'],
+                savefilesuffix = "testing"  ))
+
+models.append(DynAERNN(d   = dim_emb,
+            beta           = 5,
+            n_prev_graphs  = 2,
+            nu1            = 1e-6,
+            nu2            = 1e-6,
+            n_aeunits      = [500, 300],
+            n_lstmunits    = [500,dim_emb],
+            rho            = 0.3,
+            n_iter         = 250,
+            xeta           = 1e-3,
+            n_batch        = 100,
+            modelfile      = ['./intermediate/enc_model_dynAERNN.json', 
+                              './intermediate/dec_model_dynAERNN.json'],
+            weightfile     = ['./intermediate/enc_weights_dynAERNN.hdf5', 
+                              './intermediate/dec_weights_dynAERNN.hdf5'],
+            savefilesuffix = "testing"
+        ))
+
 # For each model, learn the embedding and evaluate on graph reconstruction and visualization
 for embedding in models:
     print ('Num nodes: %d, num edges: %d' % (G.number_of_nodes(), G.number_of_edges()))
