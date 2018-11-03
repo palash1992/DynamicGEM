b7048b5b60b268319ead808b5f44a9cf341f167f
schhetri <schhetri@uci.edu>
2018-11-03 15:40:32 -0700

    output changed

--- example/example.py
+++ example/example.py
@@ -71,9 +71,10 @@ t1 = time()
 for temp_var in range(length):
     emb, _= embedding.learn_embeddings(graphs[temp_var])
     embs.append(emb)
-    print(embedding._method_name,"---Training time:", (time() - t1))
+print(embedding._method_name,"---Training time:", (time() - t1))
 viz.plot_static_sbm_embedding(embs[-4:], list(dynamic_sbm_series)[-4:])   
-
+plt.savefig('./'+outdir+'/ae_static.png',bbox_inches='tight',dpi=600) 
+plt.show() 
 
 #dynamicTriad------------
 datafile  = dataprep_util.prep_input_dynTriad(graphs, length, testDataType)
